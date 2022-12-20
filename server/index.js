ec31430aefb55042741a916f7e0cecd9682526ca
Maizied Hasan <mdshuvo40@gmail.com>
2022-12-20 17:50:05 +0600

    Added sequelize and nodemon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const db = require('./models');
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001");
+db.sequelize.sync().then(() =>{
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
+    });
 });
