ee79839f649b1dde24651a9b3663e4b5e548f316
Maizied Hasan <mdshuvo40@gmail.com>
2023-01-08 18:37:13 +0600

    Registration api completed

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { Users } = require("../models");
+const bcrypt = require("bcrypt");
 
 
 router.post("/", async(req, res) => {
-   const {username, password} = req.body;
+   const {username, email, password} = req.body;
+   bcrypt.hash(password,10).then((hash)=>{
+    Users.create({
+        username: username,
+        email: email,
+        password: hash
+    });
+    res.json("Success!")
+   });
 });
 
 
